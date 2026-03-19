c6429748884e8063b68571c875cef80c297a1358
ichiyo <me@ichiyo.in>
2026-03-19 20:20:08 +0800

    fix: disable experimental rolldown plugin to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
+// import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 
@@ -9,7 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    // babel({ presets: [reactCompilerPreset()] })
   ],
   base: '/sidereus-web/',
 })
